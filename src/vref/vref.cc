aa087d47b5f8def2e09d01adb9b5bf73085ff872
Damien Petit <damien.gerard.petit@gmail.com>
2013-09-13 12:15:01 +0900

    add control law to realign the robot toward the target

--- src/vref/vref.cc
+++ src/vref/vref.cc
@@ -5,6 +5,8 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
 
+#include <math.h>
+
 class Vref
 {
 public:
@@ -13,10 +15,17 @@ public:
       nodeHandlePrivate_ ("~"),
       queueSize_ (queueSize),
       objectPositionFilteredSubscriber_ (),
-      vrefPublisher_ ()
+      vrefPublisher_ (),
+      rotationToObjectRef_(0.0),//rad
+      kRotInitThreshMaxDeg_(15.0),//degree
+      rotInitMaxVel_(0.2),//rad/s
+      kRotInit_(0.0),//1/s
+      vRefRotInitDeadZone_(5.0)//deg
   {
     typedef boost::function<void (const geometry_msgs::PoseStampedConstPtr&)>
       callback_t;
+
+    kRotInit_ = rotInitMaxVel_*180/(kRotInitThreshMaxDeg_*M_PI);
 
     callback_t callback =
       boost::bind (&Vref::callback, this, _1);
@@ -45,8 +54,49 @@ public:
       }
   }
 
+  void computeRotationToObjectRef(const geometry_msgs::PoseStampedConstPtr& objectPosition, double& rotationToObjectRef)
+  {
+    rotationToObjectRef=(atan2( objectPosition->pose.position.y, objectPosition->pose.position.x));
+  }
+
+  void computeVRefRotInit(const double& rotationToObjectRef, double& vRefRot)
+  {
+    double vrefRotTemp = 0.0;
+    //deadzone of rotationToObjectRef
+    //if the error is small there is no need to move
+    if( abs(rotationToObjectRef) > (vRefRotInitDeadZone_*M_PI/180))
+    {
+        vrefRotTemp = -1*kRotInit_*rotationToObjectRef;
+        //gap the result
+        if(vrefRotTemp > rotInitMaxVel_)
+        {
+            vrefRotTemp = rotInitMaxVel_;
+        }
+        else
+        {
+        }
+
+        if(vrefRotTemp < -1*rotInitMaxVel_)
+        {
+            vrefRotTemp = -1*rotInitMaxVel_;
+        }
+        else
+        {
+        }
+    }
+    else
+    {
+        vrefRotTemp = 0.0;
+    } 
+
+    vRefRot = vrefRotTemp;
+  }
+
+//objectPosition is the pose in the waist of the robot
   void callback (const geometry_msgs::PoseStampedConstPtr& objectPosition)
   {
+    computeRotationToObjectRef( objectPosition , rotationToObjectRef_);
+
     geometry_msgs::Vector3StampedPtr vref =
       boost::make_shared<geometry_msgs::Vector3Stamped> ();
 
@@ -66,6 +116,17 @@ private:
 
   ros::Subscriber objectPositionFilteredSubscriber_;
   ros::Publisher vrefPublisher_;
+
+  double rotationToObjectRef_;
+
+  //variables used in the realignment initial motion
+  double kRotInitThreshMaxDeg_;
+  double rotInitMaxVel_;
+  double kRotInit_;
+  double vRefRotInitDeadZone_;
+
+  //variable used for the circle around motion
+
 };
 
 int main(int argc, char **argv)
